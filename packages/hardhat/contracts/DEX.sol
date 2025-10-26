516f083db3279a0cf7897467d0a17e915001949d
Silki <129131071+silkiy@users.noreply.github.com>
2025-10-26 13:35:51 +0700

    feat : init dex

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -13,6 +13,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  */
 contract DEX {
     /* ========== GLOBAL VARIABLES ========== */
+    uint256 public totalLiquidity;
+    mapping(address => uint256) public liquidity;
 
     IERC20 token; //instantiates the imported contract
 
@@ -57,13 +59,24 @@ contract DEX {
      * @return totalLiquidity is the number of LPTs minting as a result of deposits made to DEX contract
      * NOTE: since ratio is 1:1, this is fine to initialize the totalLiquidity (wrt to balloons) as equal to eth balance of contract.
      */
-    function init(uint256 tokens) public payable returns (uint256) {}
+    function init(uint256 tokens) public payable returns (uint256) {
+        require(totalLiquidity == 0, "DEX already initialized");
+        totalLiquidity = address(this).balance;
+        liquidity[msg.sender] = totalLiquidity;
+        require(token.transferFrom(msg.sender, address(this), tokens), "Token transfer failed");
+        return totalLiquidity;
+    }
 
     /**
      * @notice returns yOutput, or yDelta for xInput (or xDelta)
      * @dev Follow along with the [original tutorial](https://medium.com/@austin_48503/%EF%B8%8F-minimum-viable-exchange-d84f30bd0c90) Price section for an understanding of the DEX's pricing model and for a price function to add to your contract. You may need to update the Solidity syntax (e.g. use + instead of .add, * instead of .mul, etc). Deploy when you are done.
      */
-    function price(uint256 xInput, uint256 xReserves, uint256 yReserves) public pure returns (uint256 yOutput) {}
+    function price(uint256 xInput, uint256 xReserves, uint256 yReserves) public pure returns (uint256 yOutput) {
+        uint256 inputWithFee = xInput * 997;
+        uint256 numerator = inputWithFee * yReserves;
+        uint256 denominator = (xReserves * 1000) + inputWithFee;
+        return numerator / denominator;
+    }
 
     /**
      * @notice returns liquidity for a user.
@@ -71,17 +84,33 @@ contract DEX {
      * NOTE: if you are using a mapping liquidity, then you can use `return liquidity[lp]` to get the liquidity for a user.
      * NOTE: if you will be submitting the challenge make sure to implement this function as it is used in the tests.
      */
-    function getLiquidity(address lp) public view returns (uint256) {}
+    function getLiquidity(address lp) public view returns (uint256) {
+        return liquidity[lp];
+    }
 
     /**
      * @notice sends Ether to DEX in exchange for $BAL
      */
-    function ethToToken() public payable returns (uint256 tokenOutput) {}
+    function ethToToken() public payable returns (uint256 tokenOutput) {
+        uint256 tokenReserve = token.balanceOf(address(this));
+        tokenOutput = price(msg.value, address(this).balance - msg.value, tokenReserve);
+        require(token.transfer(msg.sender, tokenOutput), "Token transfer failed");
+        emit EthToTokenSwap(msg.sender, tokenOutput, msg.value);
+        return tokenOutput;
+    }
 
     /**
      * @notice sends $BAL tokens to DEX in exchange for Ether
      */
-    function tokenToEth(uint256 tokenInput) public returns (uint256 ethOutput) {}
+    function tokenToEth(uint256 tokenInput) public returns (uint256 ethOutput) {
+        uint256 tokenReserve = token.balanceOf(address(this));
+        ethOutput = price(tokenInput, tokenReserve, address(this).balance);
+        require(token.transferFrom(msg.sender, address(this), tokenInput), "Token transfer failed");
+        (bool success, ) = payable(msg.sender).call{ value: ethOutput }("");
+        require(success, "ETH transfer failed");
+        emit TokenToEthSwap(msg.sender, tokenInput, ethOutput);
+        return ethOutput;
+    }
 
     /**
      * @notice allows deposits of $BAL and $ETH to liquidity pool
@@ -89,11 +118,39 @@ contract DEX {
      * NOTE: user has to make sure to give DEX approval to spend their tokens on their behalf by calling approve function prior to this function call.
      * NOTE: Equal parts of both assets will be removed from the user's wallet with respect to the price outlined by the AMM.
      */
-    function deposit() public payable returns (uint256 tokensDeposited) {}
+    function deposit() public payable returns (uint256 tokensDeposited) {
+        uint256 ethReserve = address(this).balance - msg.value;
+        uint256 tokenReserve = token.balanceOf(address(this));
+        uint256 tokenAmount = (msg.value * tokenReserve) / ethReserve;
+        uint256 liquidityMinted = (msg.value * totalLiquidity) / ethReserve;
+
+        liquidity[msg.sender] += liquidityMinted;
+        totalLiquidity += liquidityMinted;
+
+        require(token.transferFrom(msg.sender, address(this), tokenAmount), "Token transfer failed");
+        emit LiquidityProvided(msg.sender, liquidityMinted, msg.value, tokenAmount);
+        return tokenAmount;
+    }
 
     /**
      * @notice allows withdrawal of $BAL and $ETH from liquidity pool
      * NOTE: with this current code, the msg caller could end up getting very little back if the liquidity is super low in the pool. I guess they could see that with the UI.
      */
-    function withdraw(uint256 amount) public returns (uint256 ethAmount, uint256 tokenAmount) {}
+    function withdraw(uint256 amount) public returns (uint256 ethAmount, uint256 tokenAmount) {
+        require(liquidity[msg.sender] >= amount, "Not enough liquidity");
+        uint256 ethReserve = address(this).balance;
+        uint256 tokenReserve = token.balanceOf(address(this));
+
+        ethAmount = (amount * ethReserve) / totalLiquidity;
+        tokenAmount = (amount * tokenReserve) / totalLiquidity;
+
+        liquidity[msg.sender] -= amount;
+        totalLiquidity -= amount;
+
+        (bool success, ) = payable(msg.sender).call{ value: ethAmount }("");
+        require(success, "ETH transfer failed");
+        require(token.transfer(msg.sender, tokenAmount), "Token transfer failed");
+
+        emit LiquidityRemoved(msg.sender, amount, tokenAmount, ethAmount);
+    }
 }
